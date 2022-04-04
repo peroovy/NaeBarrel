927bb3d31fa0bed0eccb50a98e57f4e9a36363d1
Yura Perov <iury.perov@gmail.com>
2022-04-04 13:24:12 +0500

    Fixed naebarrel.sql. Added init for inventories

--- test_db/naebarrel.sql
+++ test_db/naebarrel.sql
@@ -1,32 +1,40 @@
-INSERT INTO public.qualities (id, name) VALUES (3, 'common');
-INSERT INTO public.qualities (id, name) VALUES (4, 'uncommon');
+INSERT INTO qualities (name) VALUES ('common');
+INSERT INTO qualities (name) VALUES ('uncommon');
 
-INSERT INTO public.transaction_types (id, name) VALUES (1, 'Продажа предмета');
-INSERT INTO public.transaction_types (id, name) VALUES (3, 'Покупка кейса');
-INSERT INTO public.transaction_types (id, name) VALUES (2, 'Ежедневное получение');
+INSERT INTO transaction_types (name) VALUES ('Продажа предмета');
+INSERT INTO transaction_types (name) VALUES ('Покупка кейса');
+INSERT INTO transaction_types (name) VALUES ('Ежедневное получение');
 
-INSERT INTO public.permissions (id, name) VALUES (1, 'admin');
-INSERT INTO public.permissions (id, name) VALUES (2, 'moderator');
-INSERT INTO public.permissions (id, name) VALUES (3, 'user');
+INSERT INTO permissions (name) VALUES ('admin');
+INSERT INTO permissions (name) VALUES ('moderator');
+INSERT INTO permissions (name) VALUES ('user');
 
-INSERT INTO public.clients (id, login, password, email, permission, balance) VALUES (1, 'nikitasamkov', '123', 'nick.samkov@yandex.ru', 1, 500);
+INSERT INTO clients (login, password, email, permission, balance) VALUES ('nikitasamkov', '123', 'nick.samkov@yandex.ru', 1, 500);
+INSERT INTO clients (login, password, email, permission, balance) VALUES ('matvey', '007', null, 2, 500);
+INSERT INTO clients (login, password, email, permission, balance) VALUES ('thetmgaming', '228', 'iurik.pro@nikita.style', 3, 100);
 
-INSERT INTO public.transactions (id, type, client_id, accrual) VALUES (1, 1, 1, 150);
-INSERT INTO public.transactions (id, type, client_id, accrual) VALUES (2, 3, 1, -150);
-INSERT INTO public.transactions (id, type, client_id, accrual) VALUES (3, 2, 1, 500);
-INSERT INTO public.transactions (id, type, client_id, accrual) VALUES (4, 2, 1, -300);
+INSERT INTO transactions (type, client_id, accrual) VALUES (1, 1, 150);
+INSERT INTO transactions (type, client_id, accrual) VALUES (3, 1, -150);
+INSERT INTO transactions (type, client_id, accrual) VALUES (2, 2, 500);
+INSERT INTO transactions (type, client_id, accrual) VALUES (2, 3, -300);
 
-INSERT INTO public.cases (id, name, description, price, picture) VALUES (1, 'TestCase', 'Case for tests', 100, 'bruh');
-INSERT INTO public.cases (id, name, description, price, picture) VALUES (2, 'SecondCase', 'description', 150, 'brah');
+INSERT INTO cases (name, description, price, picture) VALUES ('TestCase', 'Case for tests', 100, 'bruh');
+INSERT INTO cases (name, description, price, picture) VALUES ('SecondCase', 'description', 150, 'brah');
 
-INSERT INTO public.items (id, name, description, price, quality, picture) VALUES (1, 'test item', 'for tests', 5, 3, 'bruh x2');
-INSERT INTO public.items (id, name, description, price, quality, picture) VALUES (2, 'second item', 'abc', 15, 3, 'sus');
-INSERT INTO public.items (id, name, description, price, quality, picture) VALUES (3, 'item #3', 'omg', 150, 3, 'picture :)');
-INSERT INTO public.items (id, name, description, price, quality, picture) VALUES (4, '4', '4 item', 1337, 3, 'wtf');
-INSERT INTO public.items (id, name, description, price, quality, picture) VALUES (5, 'magical crown', 'wow', 100500, 4, 'pretty');
+INSERT INTO items (name, description, price, quality, picture) VALUES ('test item', 'for tests', 5, 1, 'bruh x2');
+INSERT INTO items (name, description, price, quality, picture) VALUES ('second item', 'abc', 15, 1, 'sus');
+INSERT INTO items (name, description, price, quality, picture) VALUES ('item #3', 'omg', 150, 1, 'picture :)');
+INSERT INTO items (name, description, price, quality, picture) VALUES ('4', '4 item', 1337, 1, 'wtf');
+INSERT INTO items (name, description, price, quality, picture) VALUES ('magical crown', 'wow', 100500, 2, 'pretty');
 
-INSERT INTO public.case_item (case_id, item_id, chance) VALUES (1, 1, 0.5);
-INSERT INTO public.case_item (case_id, item_id, chance) VALUES (1, 2, 0.25);
-INSERT INTO public.case_item (case_id, item_id, chance) VALUES (1, 3, 0.25);
-INSERT INTO public.case_item (case_id, item_id, chance) VALUES (2, 1, 0.1);
-INSERT INTO public.case_item (case_id, item_id, chance) VALUES (2, 4, 0.9);
+INSERT INTO case_item (case_id, item_id, chance) VALUES (1, 1, 0.5);
+INSERT INTO case_item (case_id, item_id, chance) VALUES (1, 2, 0.25);
+INSERT INTO case_item (case_id, item_id, chance) VALUES (1, 3, 0.25);
+INSERT INTO case_item (case_id, item_id, chance) VALUES (2, 1, 0.1);
+INSERT INTO case_item (case_id, item_id, chance) VALUES (2, 4, 0.9);
+
+INSERT INTO inventories (client_id, item_id) VALUES (1, 1);
+INSERT INTO inventories (client_id, item_id) VALUES (1, 2);
+INSERT INTO inventories (client_id, item_id) VALUES (1, 3);
+INSERT INTO inventories (client_id, item_id) VALUES (3, 4);
+INSERT INTO inventories (client_id, item_id) VALUES (3, 5);

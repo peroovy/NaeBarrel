b9aadd5c8bbc9584923f255c19e2349e09c11298
Yura Perov <iury.perov@gmail.com>
2022-06-10 03:24:48 +0500

    Fixed auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        try
+        {
+            $this->authenticate($request, $guards);
+
+            return $next($request);
+        }
+        catch (AuthenticationException $exception)
+        {
+            return response(status: 401);
         }
     }
 }
